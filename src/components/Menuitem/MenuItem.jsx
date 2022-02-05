1f06a322bd82c315a017d55b7eb35464f35d7151
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2022-02-05 20:02:37 +0200

    docs: update Special Menu section on SpecialMenu.css, MenuItem.css, MenuItem.jsx

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({title, price, tags}) => (
+  <div className='app__MenuItem'>
+    <div className="app__MenuItem-head">
+		<div className="app__MenuItem-name">
+			<p className='p__cormorant' style={{ color: '#DCCA87'}}>{title}</p>
+		</div>
+
+		<div className="app__MenuItem-price">
+			<p className='p__cormorant' >{price}</p>
+		</div>
+	</div>
   </div>
 );
 
