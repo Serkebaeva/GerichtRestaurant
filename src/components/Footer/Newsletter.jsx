74619f41a9f13a8b3f48998ad9dd8698da20a1a8
AizadaSerkebaeva <aizada.serkebaeva.pls@gmail.com>
2022-02-12 18:50:10 +0200

    docs: update gallery.jsx bug fix

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import SubHeading from '../SubHeading/SubHeading'
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newsletter'>
+    <div className='app__newsletter-heading'>
+		<SubHeading title='Newsletter'/>
+		<h1 className='headtext__cormorant'>Subscribe to Our Newsletter</h1>
+		<p className='p__opensans'>And never miss the latest updates</p>
+	</div>
   </div>
 );
 
--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -5,35 +5,33 @@ import { SubHeading} from '../../components';
 import { images } from '../../constants';
 import './Gallery.css';
 
-const GalleryImages = [images.gallery01, images.gallery02, images.gallery03, images.gallery04];
-
 const Gallery = () => {
   const scrollRef = React.useRef(null);
 	
   const scroll = (direction) => {
-	  const {current} = scrollRef;
+	  const { current } = scrollRef;
 
 	  if (direction === 'left') {
 		  current.scrollLeft -= 300;
 	  } else {
-		  current.scrollRight -=300;
+		  current.scrollLeft +=300;
 	  }
-  }
+  };
 
   return (
-	<div className='app__gallery flex-center'>
+	<div className="app__gallery flex__center">
 		<div className='app__gallery-content'>
 			<SubHeading title='Instagram' />
 			<h1 className='headtext__cormorant'>Photo Gallery</h1>
-			<p className='p__opensans' style={{color: '#AAA', marginTop: '2rem'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, saepe fugit consectetur hic tenetur!</p>
+			<p className='p__opensans' style={{color: '#AAAAAA', marginTop: '2rem'}}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, saepe fugit consectetur hic tenetur!</p>
 			<button type='button' className='custom__button'>View More</button>
 		</div>
 
 		<div className='app__gallery-images'>
 			<div className='app__gallery-images_container' ref={scrollRef}>
-				{Gallery.images.map((image, index) => (
+				{[images.gallery01, images.gallery02, images.gallery03, images.gallery04].map((image, index) => (
 					<div className='app__gallery-images_card flex__center' key={`gallery_image-${index + 1}`}>
-						<img src={image} alt="gallery" />
+						<img src={image} alt="gallery_image" />
 						<BsInstagram className='gallery__image-icon' />
 					</div>
 				))}
